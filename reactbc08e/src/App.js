c8622c1da10f768d065bc80f6f8d6637bdffb54b
Phúc Lê <permees@Phucs-Air.lan>
2021-06-23 22:27:30 +0700

    create app

---
+++ reactbc08e/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Fragment } from "react";
+// import HomeComponents from "./Components/HomeComponent";
+// import DataBiding from "./DataBiding/DataBiding";
+
+function App() {
+  return (
+    <Fragment>
+      {/* <HomeComponents /> */}
+      {/* {<DataBiding /> */}
+    </Fragment>
+  );
+}
+
+export default App;
